78ab2827e7b35f6e30276b3b0bfa76f5303af669
thdiaman <themisdiamantopoulos@hotmail.com>
2018-10-11 01:34:55 +0300

    Update naive bayes dataset

--- NaiveBayes/naive_bayes_full.py
+++ NaiveBayes/naive_bayes_full.py
@@ -6,18 +6,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, roc_curve, precision_recall_curve, auc
 
 # Read the data
-data = pd.read_csv("data.csv", sep=';')
+data = pd.read_csv("trafficfull.csv", sep=';')
 X = data.iloc[:, 0:-1]
 y = data.iloc[:, -1]
 
 # Plot the data
-#fig, ax = plt.subplots()
-#groups = data.groupby('Y')
-#for name, group in groups:
-#    ax.plot(group.iloc[:, 0], group.iloc[:, 1], marker='o', linestyle='', label=name)
-#ax.set_xlabel('X1')
-#ax.set_ylabel('X2')
-#ax.legend()
+fig, ax = plt.subplots()
+groups = data.groupby('HighTraffic')
+for name, group in groups:
+    ax.plot(group.iloc[:, 0], group.iloc[:, 1], marker='o', linestyle='', label=name)
+ax.set_xlabel('Temperature')
+ax.set_ylabel('Wind')
+ax.legend()
 
 # Split the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
